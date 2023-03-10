23f2abe73d322b8417f3bdf0142cf16f417aa1f4
MatangGooree <gmlwosla42@gmail.com>
2023-03-10 20:28:13 +0900

    origin

--- server.js
+++ server.js
@@ -21,15 +21,11 @@ MongoClient.connect(process.env.DB_URL, { useUnifiedTopology: true }, function (
 });
 
 app.get('/', function (req, res) {
-  res.render('index.ejs');
+  res.render('schedule.ejs');
 });
 
 app.get('/schedule', function (req, res) {
-  db.collection('workers')
-    .find()
-    .toArray(function (err, data) {
-      res.render('index.ejs',{worker:data});
-    });
+  res.render('schedule.ejs');
 });
 
 app.get('/vacation', function (req, res) {
@@ -40,8 +36,47 @@ app.get('/vacation', function (req, res) {
     });
 });
 
-app.post('/vacation_req', function (req, res) {
-  db.collection('vacation').insertOne({ year: req.body.year, month: req.body.month, day: req.body.day, worker: req.body.worker }, function (err, data) {
-    res.redirect('/vacation')
-  });
+app.get('/data/workers', function (req, res) {
+  db.collection('workers')
+    .find()
+    .toArray(function (err, data) {
+      var workers = [];
+
+      for (let i = 0; i < data.length; i++) {
+        workers.push(data[i].worker);
+      }
+      res.json(JSON.stringify(workers));
+    });
 });
+
+app.post('/vacation_req', function (req, res) {
+  console.log('신청 수신');
+  req.body.year = parseInt(req.body.year);
+  req.body.month = parseInt(req.body.month);
+  req.body.day = parseInt(req.body.day);
+
+  db.collection('vacation').insertOne(
+    { year: req.body.year, month: req.body.month, day: req.body.day, worker: req.body.worker, time: req.body.time, changed: req.body.changed_worker },
+    function (err, data) {
+      res.redirect('/vacation');
+    }
+  );
+});
+
+app.get('/data/vac_req', function (req, res) {
+  db.collection('vacation')
+    .find()
+    .toArray(function (err, data) {
+      res.json(JSON.stringify(data));
+    });
+});
+
+//로그인기능
+
+//휴가취소 버튼
+
+//휴가갯수 카운트
+
+//24시간 풀근무 못하도록
+
+//본인 휴가신청 버튼만 보이도록
